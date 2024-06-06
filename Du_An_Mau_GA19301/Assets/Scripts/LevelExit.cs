0004183f0e7ac2b60b3e6cf5f91c75c674b20364
ThMii <tranbaothong1704@gmail.com>
2024-06-06 07:56:03 +0700

    Thong

---
+++ Du_An_Mau_GA19301/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class LevelExit : MonoBehaviour
+    {
+        [SerializeField] float levelLoadDelay;
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Player"))
+            {
+                StartCoroutine(LoadNextLevel());
+            }
+
+            StartCoroutine(LoadNextLevel());
+        }
+
+        IEnumerator LoadNextLevel()
+        {
+            yield return new WaitForSecondsRealtime(levelLoadDelay);
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
+
+            if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            {
+                nextSceneIndex = 0;
+            }
+
+            FindObjectOfType<ScenePersist>().ResetScreenPersist();
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
